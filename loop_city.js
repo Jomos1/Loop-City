0a4f337d9751df15585b3ddc44f3cb42be24b32d
Student <idk123@gmail.com>
2016-11-28 08:43:26 -1000

    modified:   loop_city.js

---
+++ loop_city.js
@@ -0,0 +1,11 @@
+for (var count = 0; <= 100; count++){
+	console.log(count);
+}
+
+var animalCount; 
+
+for (count = 0; <= 5; count++){
+	console.log(count, "there are" + count + " many animals in noah's ")
+}
+
+var partyAnimals = [lion,zebra,cow,deer,nuke]
