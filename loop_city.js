87a3187c17951a0c5639398b1e428160ab9a94fe
Student <idk123@gmail.com>
2016-11-30 09:21:43 -1000

    modified:   loop_city.js

--- loop_city.js
+++ loop_city.js
@@ -1,11 +1,31 @@
-for (var count = 0; <= 100; count++){
+for (var count = 0; count < 100; count++){
 	console.log(count);
 }
 
-var animalCount; 
-
-for (count = 0; <= 5; count++){
-	console.log(count, "there are" + count + " many animals in noah's ")
+var animalCount = null
+for (count = 0; count <=  5; count++){
+	console.log(count, "there are " + count + " many animals in noah's arc ");
 }
 
-var partyAnimals = [lion,zebra,cow,deer,nuke]
+
+var partyAnimals = ["lion","zebra","cow","deer","nuke"]
+for (animals = 0; animals < 5; animals++){
+	console.log(partyAnimals[animals] + " is savage");
+}
+
+var waepunChast = ["Masse", "Soorusu Sutone", "Kirito"]
+var heero = {
+	weebzard: "Myleen",
+	waepun: waepunChast[1],
+	POWAH: 30,
+}
+function shmoto (hammahdolo){
+	if (heero.waepun == "Soorusu Sutone"){
+		console.log(heero.weebzard + " shall slay teh Dragoon wit " + heero.waepun);
+	}
+	else {
+		console.log(heero.weebzard + "is ready for war with the" + heero.waepun);
+	}
+}
+
+shmoto(heero.waepun)
